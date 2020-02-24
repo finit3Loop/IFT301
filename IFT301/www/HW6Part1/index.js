7f92cf2256d5e1f8bb6bda3a712311fa76b20e21
Jarret Piper <12jpiper@gmail.com>
2020-02-24 05:18:56 +0000

    Update to IFT 301

--- IFT301/www/HW6Part1/index.js
+++ IFT301/www/HW6Part1/index.js
@@ -1,10 +1,17 @@
+//find all div elements
 var cards = document.getElementsByTagName("div");
+
+//iterate through all divs selecting only the correct ones to apply card class to
 for(var i = 0; i < cards.length; i++){
    if(cards[i].id == "card"){
     cards[i].classList.add("card");
    }
 }
+
+//find all ul elements
 var attributes = document.getElementsByTagName("ul");
+
+//iterate through all uls selecting only those for the attributes class
 for(var i = 0; i < attributes.length; i++){
    if(attributes[i].id == "attributes"){
     attributes[i].classList.add("attributes");
