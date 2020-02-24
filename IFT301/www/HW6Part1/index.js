9515a2c5225a68c233b9d5dd8681ca44a8866ad0
Jarret Piper <12jpiper@gmail.com>
2020-02-24 04:30:27 +0000

    Update to IFT 301

--- IFT301/www/HW6Part1/index.js
+++ IFT301/www/HW6Part1/index.js
@@ -1,27 +1,9 @@
-seasonText = document.getElementById("youChoose")
-seasonImg = document.getElementById("seasonImg")
+var cards = document.getElementsByTagName("div");
+for(var i = 0; i < cards.length; i++){
+   if(cards[i].id == "card"){
+    cards[i].classList.add("card");
+   }
+}
 
 
-var favSeason = prompt("What's your favorite season?").toLowerCase();
-switch(favSeason) {
-    case "winter":
-        seasonText.textContent = "You Choose Winter";
-        seasonImg.src = "./winter.jpg"
-        break;
-    case "spring":
-        seasonText.textContent = "You Choose Spring";
-        seasonImg.src = "./spring.jpg"
-        break;
-    case "summer":
-        seasonText.textContent = "You Choose Summer";
-        seasonImg.src = "./summer.jpg"
-        break;
-    case "fall":
-        seasonText.textContent = "You Choose Fall";
-        seasonImg.src = "./fall.jpg"
-        break;
-    default:
-        seasonText.textContent = "I have never heard of that season...";
-        seasonImg.src = "./question.png"
-        break;
-} 
+
