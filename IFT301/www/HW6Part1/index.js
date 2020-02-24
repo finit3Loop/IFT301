60743524f752db16f6191b658c81cf40e4500d42
Jarret Piper <12jpiper@gmail.com>
2020-02-24 06:10:32 +0000

    Update to IFT 301

--- IFT301/www/HW6Part1/index.js
+++ IFT301/www/HW6Part1/index.js
@@ -17,3 +17,30 @@ for(var i = 0; i < attributes.length; i++){
     attributes[i].classList.add("attributes");
    }
 }
+var campButton = document.getElementById("campaignButton");
+campButton.addEventListener("click", campaignLinks);
+
+//function for adding campaign links
+function campaignLinks(){
+
+    //add link for Joe
+    var joe = document.getElementById("Joe");
+    var wrapper = document.createElement('a');
+    wrapper.setAttribute('href', 'https://joebiden.com/');
+    wrapper.appendChild(joe.cloneNode(true));
+    joe.parentNode.replaceChild(wrapper, joe);
+
+    //add link for Bernie
+    var bern = document.getElementById("Bernie");
+    var wrapper = document.createElement('a');
+    wrapper.setAttribute('href', 'https://berniesanders.com/');
+    wrapper.appendChild(bern.cloneNode(true));
+    bern.parentNode.replaceChild(wrapper, bern);
+
+    //add link for Elizabeth
+    var liz = document.getElementById("Elizabeth");
+    var wrapper = document.createElement('a');
+    wrapper.setAttribute('href', 'https://elizabethwarren.com/');
+    wrapper.appendChild(liz.cloneNode(true));
+    liz.parentNode.replaceChild(wrapper, liz);
+}
