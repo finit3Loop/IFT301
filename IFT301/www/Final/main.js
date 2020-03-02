0a23ee8281b424ac77e131846a5f5408b36ab26e
Jarret Piper <12jpiper@gmail.com>
2020-03-01 21:03:09 -0600

    Update to IFT 301

--- IFT301/www/Final/main.js
+++ IFT301/www/Final/main.js
@@ -1,36 +1,71 @@
-function addCourt() {
-    var courtDiv = document.getElementById("game-grid");
+var fnArray = [];
+var lnArray = [];
+var locationArray = [];
 
+console.log(fnArray.length);
+
+
+for(var i = 0; i < length.fnArray; i++){
+    
+    var gameGrid = getElementById("game-grid");
+    
+    var gameUl = document.createElement('ul');
+    gameUl.setAttribute('id', 'game-ul')
+
+
+    var firstnameLi = document.createElement('li');
+    firstnameLi.appendChild(fnArray[i]);
+    
+    var lastnameLi = document.createElement('li');
+    lastnameLi.appendChild(lnArray[i]);
+    
+    var locationLi = document.createElement('li');
+    locationLi.appendChild(locatonArray[i]);
+
+    gameUl.appendChild(firstnameLi);
+    gameUl.appendChild(lastnameLi);
+    gameUl.appendChild(locaitonLi);
+
+    gameUl.appendChild(gameGrid.cloneNode(true));
+    gameGrid.parentNode.replaceChilde(gameUl,gameGrid);
+
+}
+
+
+
+
+
+
+
+function addCourt() {
 
 
     var gameGrid = document.getElementsByClassName("game-grid");
-    var firstname = document.getElementsByName("firstname");
-    var lastname = document.getElementsByName("lastname");
-    var location = document.getElementsByName("location");
+    var firstname = document.getElementById("submit-form").elements.namedItem("firstname").value;
+    var lastname = document.getElementById("submit-form").elements.namedItem("lastname").value;
+    var location = document.getElementById("submit-form").elements.namedItem("location").value;
 
     console.log(firstname);
     console.log(lastname);
     console.log(location);
 
 
-
+    var courtWrapper = document.createElement('ul');
+    
     var fnWrapper = document.createElement('ul');
     var firstnameLi = document.createElement('li');
-    firstnameLi.appendChild(firstname);
-    courtDiv.appendChild(fnWrapper);
+    firstnameLi.innerHTML = firstname;
+    fnWrapper.appendChild(firstnameLi);
 
     var lnWrapper = document.createElement('ul');
     var lastnameLi = document.createElement('li');
-    lastnameLi.appendChild(lastname);
-    courtDiv.appendChild(lnWrapper);
+    lastnameLi.innerHTML = lastname;
+    lnWrapper.appendChild(lastnameLi);
 
-    var lnWrapper = document.createElement('ul');
-    var lastnameLi = document.createElement('li');
-    lastnameLi.appendChild(lastname);
-    courtDiv.appendChild(lnWrapper);
-    
     var locationWrapper = document.createElement('ul');
     var locationLi = document.createElement('li');
-    locationLi.appendChild(location);
-    courtDiv.appendChild(locationWrapper);
+    locationLi.innerHTML = location;
+    locationWrapper.appendChild(locationLi);
+    
+    
 }
