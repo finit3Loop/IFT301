a3b989681cb0d177e120a2f574893ad63fc8fd26
Jarret Piper <12jpiper@gmail.com>
2020-03-01 22:04:01 -0600

    Update to IFT 301

--- IFT301/www/Final/main.js
+++ IFT301/www/Final/main.js
@@ -2,35 +2,45 @@
 function addCourt() {
 
 
-    var gameGrid = document.getElementsByClassName("game-grid");
+    var gameGrid = document.getElementById("game-grid");
     var firstname = document.getElementById("submit-form").elements.namedItem("firstname").value;
     var lastname = document.getElementById("submit-form").elements.namedItem("lastname").value;
     var location = document.getElementById("submit-form").elements.namedItem("location").value;
+    var discription = document.getElementById("submit-form").elements.namedItem("discription").value;
 
-    console.log(firstname);
-    console.log(lastname);
-    console.log(location);
+    firstname = "First Name: " + firstname;
+    lastname = "Last Name: " + lastname;
+    location = "Location: " + location;
 
 
-    var courtWrapper = document.createElement('ul');
+
+    var courtWrapper = document.createElement('div');
+    courtWrapper.setAttribute('class','game-card');
     
-    var fnWrapper = document.createElement('ul');
-    var firstnameLi = document.createElement('li');
+    var firstnameLi = document.createElement('h4');
     firstnameLi.innerHTML = firstname;
-    fnWrapper.appendChild(firstnameLi);
-
-    var lnWrapper = document.createElement('ul');
-    var lastnameLi = document.createElement('li');
-    lastnameLi.innerHTML = lastname;
-    lnWrapper.appendChild(lastnameLi);
-
-    var locationWrapper = document.createElement('ul');
-    var locationLi = document.createElement('li');
-    locationLi.innerHTML = location;
-    locationWrapper.appendChild(locationLi);
     
-    courtWrapper.appendChild(fnWrapper);
-    courtWrapper.appendChild(lnWrapper);
-    courtWrapper.appendChild(locationWrapper);
 
+    
+    var lastnameLi = document.createElement('h4');
+    lastnameLi.innerHTML = lastname;
+    
+
+    
+    var locationLi = document.createElement('p');
+    locationLi.innerHTML = location;
+    
+
+    var discriptionLi = document.createElement('p');
+    discriptionLi.innerHTML = discription;
+    
+    courtWrapper.appendChild(firstnameLi);
+    courtWrapper.appendChild(lastnameLi);
+    courtWrapper.appendChild(locationLi);
+    courtWrapper.appendChild(discriptionLi);
+    courtWrapper.appendChild(document.createElement('br'));
+
+    gameGrid.appendChild(courtWrapper);
+    //courtWrapper.appendChild(gameGrid.cloneNode(true));
+    //gameGrid.parentNode.replaceChild(courtWrapper, gameGrid);
 }
